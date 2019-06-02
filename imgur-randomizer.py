679b4ab0a57a66b54cdfba274f8c392158134c81
fire0shadow <fire0shadow@gmail.com>
2019-06-03 05:21:47 +0700

    Small fix in metavar of NUM_IMAGES argument

--- imgur-randomizer.py
+++ imgur-randomizer.py
@@ -17,7 +17,7 @@ DIR_NAME = 'results/'
 URL = 'https://i.imgur.com/'
 
 parser = argparse.ArgumentParser(description='Fetch random images from Imgur image hosting.')
-parser.add_argument('NUM_IMAGES', nargs='?', type=int, default=30, help='number of images to be fetched (default: %(default)s)')
+parser.add_argument('num_images', metavar='NUM_IMAGES', nargs='?', type=int, default=30, help='number of images to be fetched (default: %(default)s)')
 parser.add_argument('--extension', choices=FILE_EXTENSIONS, default=-1, help='fetch images by specified extension (default: random)')
 parser.add_argument('--name_length', type=int, choices=range(MIN_NAME_LENGTH, MAX_NAME_LENGTH+1), default=-1, help='file name length, 7-characters names are rare, so it’s better to use 6 or 7 (default: random)')
 parser.add_argument('--min_width', type=int, default=100, help='minimum width of fetched image in pixels (default: %(default)s)')
