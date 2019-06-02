5a1f41b2d5e45fde66749bd5f64f9ab385110d74
fire0shadow <fire0shadow@gmail.com>
2019-06-03 05:41:00 +0700

    Removed "file extension" argument, added file format identifying using Pillow

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # imgur-randomizer
 ```
-usage: imgur-randomizer.py [-h] [--extension {jpg,png,gif,mov,mp4}] [--name_length {5,6,7}] [--min_width MIN_WIDTH] [--min_height MIN_HEIGHT] [NUM_IMAGES]
+usage: imgur-randomizer.py [-h] [--name_length {5,6,7}] [--min_width MIN_WIDTH] [--min_height MIN_HEIGHT] [NUM_IMAGES]
 
 Fetch random images from Imgur image hosting.
 
@@ -9,9 +9,6 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit
-
-  --extension {jpg,png,gif,mov,mp4}
-                        fetch images by specified extension (default: random)
 
   --name_length {5,6,7}
                         file name length, 7-characters names are rare, so it’s better to use 6 or 7 (default: random)
--- imgur-randomizer.py
+++ imgur-randomizer.py
@@ -8,7 +8,6 @@ import argparse
 import io
 from PIL import Image
 
-FILE_EXTENSIONS = ['jpg', 'png', 'gif', 'mov', 'mp4']
 MIN_NAME_LENGTH = 5
 MAX_NAME_LENGTH = 7
 NAME_CHARACTERS_POOL = string.ascii_letters + string.digits
@@ -18,7 +17,6 @@ URL = 'https://i.imgur.com/'
 
 parser = argparse.ArgumentParser(description='Fetch random images from Imgur image hosting.')
 parser.add_argument('num_images', metavar='NUM_IMAGES', nargs='?', type=int, default=30, help='number of images to be fetched (default: %(default)s)')
-parser.add_argument('--extension', choices=FILE_EXTENSIONS, default=-1, help='fetch images by specified extension (default: random)')
 parser.add_argument('--name_length', type=int, choices=range(MIN_NAME_LENGTH, MAX_NAME_LENGTH+1), default=-1, help='file name length, 7-characters names are rare, so it’s better to use 6 or 7 (default: random)')
 parser.add_argument('--min_width', type=int, default=100, help='minimum width of fetched image in pixels (default: %(default)s)')
 parser.add_argument('--min_height', type=int, default=100, help='minimum height of fetched image in pixels (default: %(default)s)')
@@ -26,7 +24,6 @@ parser.add_argument('--min_height', type=int, default=100, help='minimum height 
 arguments = parser.parse_args()
 
 NUM_IMAGES = arguments.num_images
-EXTENSION = arguments.extension
 NAME_LENGTH = arguments.name_length
 MIN_WIDTH = arguments.min_width
 MIN_HEIGHT = arguments.min_height
@@ -44,30 +41,28 @@ os.mkdir(DIR_NAME)
 def random_string(length):
     return ''.join(random.choice(NAME_CHARACTERS_POOL) for i in range(length))
 
-def fetch_image(name, ext):
-	image_name = name + '.' + ext
-
-	image = requests.get(URL + image_name, allow_redirects=False)
+def fetch_image(name):
+	image = requests.get(URL + name + '.png', allow_redirects=False)
 
 	if image.status_code == 400:
-		print(image_name + ' returned bad request, skipping...')
+		print(name + ' returned bad request, skipping...')
 	elif image.status_code == 404 or image.status_code == 302:
-		print(image_name + ' doesn\'t exists or removed, skipping...')
+		print(name + ' doesn\'t exists or removed, skipping...')
 	elif image.status_code == 200:
 		image_content = image.content
 		pil_image = Image.open(io.BytesIO(image_content))
 
 		if pil_image.width < MIN_WIDTH:
-			print(image_name + ' width less than minimum, skipping...')
+			print(name + ' width less than minimum, skipping...')
 			return False
 
 		if pil_image.height < MIN_HEIGHT:
-			print(image_name + ' height less than minimum, skipping...')
+			print(name + ' height less than minimum, skipping...')
 			return False
 
-		print(image_name + ' exists, fetching...')
+		print(name + ' exists, ' + pil_image.format +' format, fetching...')
 
-		with open(DIR_NAME + image_name, 'wb') as f:
+		with open(DIR_NAME + name + '.' + pil_image.format, 'wb') as f:
 			f.write(image.content)
 
 		return True
@@ -78,7 +73,6 @@ i = 0
 while(i < NUM_IMAGES):
 	image_name_length = random.randint(MIN_NAME_LENGTH, MAX_NAME_LENGTH) if NAME_LENGTH == -1 else NAME_LENGTH
 	random_image_name = random_string(image_name_length)
-	
-	image_extension = random.choice(FILE_EXTENSIONS) if EXTENSION == -1 else EXTENSION
-	if fetch_image(random_image_name, image_extension):
+
+	if fetch_image(random_image_name):
 		i = i + 1
